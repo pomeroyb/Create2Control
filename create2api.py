f10f22d60346b95c8b1fd6384eb62e04c7f0ee88
pomeroyb <pomeroyb@uw.edu>
2015-08-13 23:23:16 -0700

    Update command functions

--- create2api.py
+++ create2api.py
@@ -35,97 +35,107 @@ class SerialCommandInterface(object):
         #This converts your nice human readable data into the necesary serial data.
         self.ser.write()
         
-class SendCommand(object):
-    """A wrapper class that holds all possible opcodes you can send
+class Create2(object):
+    """The top level class for controlling a Create2.
+        This is the only class that outside scripts should be interacting with.    
+    
     """
     
     def __init__(self):
         #Nothing yet
+        self.SCI = SerialCommandInterface()
     
     def start():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
         
     def reset():
-        #send(configData['opcodes']['reset'],0)
+        #SCI.send(configData['opcodes']['reset'],0)
         
     def stop():
-        #send(configData['opcodes']['stop'],0)
+        #SCI.send(configData['opcodes']['stop'],0)
         
     def baud():
-        #send(configData['opcodes']['baud'],0)
+        #SCI.send(configData['opcodes']['baud'],0)
     
     def safe():  ####THIS IS WHERE I GOT BORED
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def full():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def clean():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def max():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def spot():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def seek_dock():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def power():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def schedule():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def set_day_time():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def drive():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def drive_direct():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def drive_pwm():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def motors():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def motors_pwm():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def led():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def scheduling_led():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def digit_led_raw():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def buttons():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def digit_led_ascii():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def song():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def play():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def sensors():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def query_list():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def stream():
-        #send(configData['opcodes']['start'],0)
+        #SCI.send(configData['opcodes']['start'],0)
     
     def pause_resume_stream():
-        #send(configData['opcodes']['start'],0)
-    
+        #SCI.send(configData['opcodes']['start'],0)
+
+        
+
+
+
+
+
+
