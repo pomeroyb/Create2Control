26309c7a538be135720c1bf6d55192cc673f4c19
ajhatch <ajhatch15@gmail.com>
2016-06-13 00:19:51 -0400

    Builds out support for songs
    Added a bunch of new functions so the robot can play songs. Yay!

--- create2api.py
+++ create2api.py
@@ -461,14 +461,21 @@ class Create2(object):
         """
         #self.SCI.send(self.config.data['opcodes']['start'],0)
         
+ #NOTE ABOUT SONGS: For some reason you cannot play a new song immediately after playing a different one, only the first song will play. You have to time.sleep() at least a fraction of a second for the speaker to process    
+    def song(self):
+        """Not implementing this for now.
+        """
+        #test sequence
+        #self.SCI.send(self.config.data['opcodes']['start'],0)
+        
     def play_test_sound(self):
-        """written to figure out how to play sounds. this will be rewritten using proper functions as outlined already once I build those out
+        """written to figure out how to play sounds. creates a song with a playlist of notes and durations and then plays it through the speaker using a hilariously messy spread of concatenated lists
         """
         noError = True
         #sets lengths of notes
-        shortNote = 8
-        mediumNote = 16
-        longNote = 20
+        short_note = 8
+        medium_note = 16
+        long_note = 20
         
         #stores a 4 note song in song 3
         current_song = 3
@@ -478,10 +485,10 @@ class Create2(object):
         
         #writes the song note commands to play_list
         #change these to change notes
-        play_list.extend([self.config.data['midi table']['C#4'],mediumNote])
-        play_list.extend([self.config.data['midi table']['G4'],longNote])
-        play_list.extend([self.config.data['midi table']['A#3'],shortNote])
-        play_list.extend([self.config.data['midi table']['A3'],shortNote])
+        play_list.extend([self.config.data['midi table']['C#4'],medium_note])
+        play_list.extend([self.config.data['midi table']['G4'],long_note])
+        play_list.extend([self.config.data['midi table']['A#3'],short_note])
+        play_list.extend([self.config.data['midi table']['A3'],short_note])
         
         #adds up the various commands and arrays
         song_play = [self.config.data['opcodes']['play'], current_song]
@@ -493,13 +500,98 @@ class Create2(object):
         if noError:
             self.SCI.send(self.config.data['opcodes']['song'], tuple(play_sequence))
         else:
-            raise ROIFailedToSendError("Invalid data, failed to send")    
+            raise ROIFailedToSendError("Invalid data, failed to send")
             
-    def play(self):
-        """Not implementing this for now.
-        """
-        #self.SCI.send(self.config.data['opcodes']['start'],0)
     
+
+    def create_song(self,song_number,play_list):
+        """create a new song from a playlist of notes and durations and tells the robot about it. Note: no error checking for playlist accuracy (must be a series of note opcodes and durations)
+        """
+        noError = True
+        
+        #the length of the song is the length of the array divided by 2
+        song_setup = [song_number,len(play_list)/2]
+        play_list = [song_setup + play_list]
+        play_list = [val for sublist in play_list for val in sublist]
+
+        if noError:   
+            self.SCI.send(self.config.data['opcodes']['song'],tuple(play_list))
+        else:
+            raise ROIFailedToSendError("Invalid data, failed to send")
+
+    def play(self,song_number):
+        """Plays a stored song
+        """
+        noError = True
+
+        if noError:
+            self.SCI.send(self.config.data['opcodes']['play'], tuple([song_number]))
+        else:
+            raise ROIFailedToSendError("Invalid data, failed to send")
+            
+    def play_note(self,note_name,note_duration):
+        """Plays a single note by creating a 1 note song in song 0
+        """
+        current_song = 0
+        play_list=[]
+        noError = True
+        if noError:
+            #Need to map ascii to numbers from the dict.
+
+            if note_name in self.config.data['midi table']:
+                play_list.append(self.config.data['midi table'][note_name])
+                play_list.append(note_duration)
+            else:
+                # That note doesn't exist. Plays nothing
+                # Raise an error so the software knows that the input was bad
+                play_list.append(self.config.data['midi table'][0])
+                warnings.formatwarning = custom_format_warning
+                warnings.warn("Warning: Note '" + note_name + "' was not found in midi table")
+            #create a song from play_list and play it
+            self.create_song(current_song,play_list)
+            self.play(current_song)
+    
+    def play_song(self,song_number,note_string):
+        """
+        Creates and plays a new song based off a string of notes and durations. 
+        note_string - a string of notes,durations
+        for example: 'G5,16,G3,16,A#4,30'
+        """
+        #splits the string of notes and durations into two lists
+        split_list= note_string.split(',')
+        note_list = split_list[0::2]
+        duration_list = split_list[1::2]
+        #creates a list for serial codes
+        play_list = []
+        #convert the durations to integers
+        duration_list = map(int, duration_list)
+        noError = True
+        
+        print duration_list
+        print note_list
+        print len(note_list)
+        if noError:
+            #Need to map midi to numbers from the dict.
+            for i in range (0,len(note_list)):
+                #Check that the note is in the list, if it is, add it.
+                if note_list[i] in self.config.data['midi table']:
+                    play_list.append(self.config.data['midi table'][note_list[i]])
+                    play_list.append(duration_list[i])
+                else:
+                    # Note was not available. Play a rest
+                    # Raise an error so the software knows that the input was bad
+                    play_list.append(self.config.data['midi table']['rest'])
+                    play_list.append(duration_list[i])
+                    warnings.formatwarning = custom_format_warning
+                    warnings.warn("Warning: Note '" + display_string[i] + "' was not found in midi table")
+                
+            #play the song
+            print play_list
+            self.create_song(song_number,play_list)
+            self.play(song_number)
+        else:
+            raise ROIFailedToSendError("Invalid data, failed to send")
+                
     def sensors(self, packet_id):
         """Requests the OI to send a packet of sensor data bytes.
         
