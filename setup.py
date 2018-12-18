e7b2027319d1503db1b47adfeca7988713e829c4
Eliot Landrum <eliot@filament.com>
2018-12-18 14:06:58 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='create2api',
+      version='0.1',
+      description='Library for controlling the iRobot Create2',
+      url='https://github.com/pomeroyb/Create2Control',
+      author='Brandon Pomeroy',
+      author_email='brandon@pomeroyb.com',
+      license='MIT',
+      zip_safe=True)
